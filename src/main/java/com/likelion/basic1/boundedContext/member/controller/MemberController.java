801a2707bfa939663985fce216edf672fa0fea56
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-18 16:12:02 +0900

    feat: create this Object each call @RequestScope

--- src/main/java/com/likelion/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/likelion/basic1/boundedContext/member/controller/MemberController.java
@@ -8,9 +8,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.annotation.RequestScope;
 
 
 @Controller
+@RequestScope
 @AllArgsConstructor
 public class MemberController {
     private final MemberService memberService;
